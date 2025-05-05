ecfb79e9f42e4343d48b0b2e0a04d29a9093ce94
Nimil Peethambaran <nimilp@users.noreply.github.com>
2025-05-05 11:36:17 -0400

    find

--- src/main/java/com/keralakitchen/delivery/controller/UserController.java
+++ src/main/java/com/keralakitchen/delivery/controller/UserController.java
@@ -6,6 +6,10 @@ import com.keralakitchen.delivery.exception.NoUserException;
 import com.keralakitchen.delivery.exception.NoUsersException;
 import com.keralakitchen.delivery.model.User;
 import com.keralakitchen.delivery.service.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,7 @@ import java.util.Optional;
 
 @RestController()
 @RequestMapping("/users")
+@Tag(name="User Api")
 public class UserController extends BaseController{
 
     @Autowired
@@ -28,6 +33,17 @@ public class UserController extends BaseController{
     public ResponseEntity<String> getUser(@PathVariable String id) throws NoUserException {
 
         throw new NoUserException("{\"Message\":\"No user Found\",\"id\":"+id+"}");
+    }
+
+    @GetMapping("/find")
+    @Operation(summary = "Get a User By FirstName", description = "Api to Fetch users by First Name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Operation is successfully"),
+            @ApiResponse(responseCode = "404", description = "")
+    })
+    public ResponseEntity<User> getUserByFirstName(@RequestParam String firstName){
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping
--- src/main/java/com/keralakitchen/delivery/model/User.java
+++ src/main/java/com/keralakitchen/delivery/model/User.java
@@ -1,11 +1,15 @@
 package com.keralakitchen.delivery.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class User {
 
+    @Schema(name = "First Name", example = "John", requiredMode = Schema.RequiredMode.REQUIRED)
     private String firstName;
+    @Schema(name = "Last Name", example = "Doe", requiredMode = Schema.RequiredMode.REQUIRED)
     private String lastName;
+    @Schema(name = "User Type", example = "Customer", requiredMode = Schema.RequiredMode.REQUIRED)
     private String type;
 }
