c1881f0dc5c523bb77db719ab9a21aa619229eb5
Nimil Peethambaran <nimilp@users.noreply.github.com>
2025-05-21 12:57:55 -0400

    side nav & ui modification

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatSidenavModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
