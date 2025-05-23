685927c7de744f09e6bfd644dc27b6566f654d1c
Nimil Peethambaran <nimilp@users.noreply.github.com>
2025-05-23 08:38:05 -0400

    error dialog

--- ui/src/app/dialogs/error/error.component.ts
+++ ui/src/app/dialogs/error/error.component.ts
@@ -1,15 +1,57 @@
-import { Component, Inject } from "@angular/core";
-import { MAT_DIALOG_DATA, MatDialogModule } from "@angular/material/dialog";
-import { MatIcon } from "@angular/material/icon";
+// import { Component, Inject } from "@angular/core";
+// import { MAT_DIALOG_DATA, MatDialogModule } from "@angular/material/dialog";
+// import {MatButtonModule} from '@angular/material/button';
+// import { MatIcon } from "@angular/material/icon";
 
+// @Component({
+//     selector: 'error-handler-dialog',
+//     templateUrl: './global.error.handler.html',
+//     styleUrl: './global.error.handler.sass',
+//     imports:[ MatIcon, MatDialogModule ],
+//     standalone:true
+// })
+// export class ErrorHandlerDialogComponent {
+// public title = 'Network Error';
+// constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}   
+// }
+
+import {ChangeDetectionStrategy, Component, Inject, inject} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIcon } from "@angular/material/icon";
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+  MAT_DIALOG_DATA
+} from '@angular/material/dialog';
+
+/**
+ * @title Dialog elements
+ */
 @Component({
-    selector: 'error-handler-dialog',
-    templateUrl: './global.error.handler.html',
-    styleUrl: './global.error.handler.sass',
-    imports:[ MatIcon, MatDialogModule ],
-    standalone:true
+  selector: 'error-handler-dialog',
+  templateUrl: './global.error.handler.html',
+  styleUrl: './global.error.handler.sass',
+  imports: [MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatIcon],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
+// export class DialogElementsExample {
+//   readonly dialog = inject(MatDialog);
+
+//   openDialog() {
+//     this.dialog.open(ErrorHandlerDialogComponent);
+//   }
+// }
+
+// @Component({
+//   selector: 'dialog-elements-example-dialog',
+//   templateUrl: 'dialog-elements-example-dialog.html',
+//   imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+//   changeDetection: ChangeDetectionStrategy.OnPush,
+// })
 export class ErrorHandlerDialogComponent {
-public title = 'Network Error';
-constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}   
+    public title = 'Network Error';
+    constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}   
 }
